21e25ca99cedb1348c4376bd4863247469fab94a
DESKTOP-LTA825E\doanc <doancongptit@gmail.com>
2022-06-02 11:56:04 +0700

    first commit

--- index.js
+++ index.js
@@ -46,6 +46,44 @@ heading_11.innerHTML = "Exp year";
 let heading_12 = document.createElement('th');
 heading_12.innerHTML = "CVV";
 
+let fullName = document.forms["my-form"]["full-name"].value;
+let email = document.forms["my-form"]["email"].value;
+let address = document.forms["my-form"]["address"].value;
+let city = document.forms["my-form"]["city"].value;
+let state = document.forms["my-form"]["state"].value;
+let zip = document.forms["my-form"]["zip"].value;
+let nameCard = document.forms["my-form"]["name-card"].value;
+let creditCardNum = document.forms["my-form"]["credit-card-num"].value;
+let expMonth = document.forms["my-form"]["exp-month"].value;
+let expYear = document.forms["my-form"]["exp-year"].value;
+let cvv = document.forms["my-form"]["CVV"].value;
+
+function errForm() {
+    document.getElementById
+        ("name").style.border = "1px solid red";
+
+    document.getElementById
+        ("email").style.border = "1px solid red";
+
+    document.getElementById
+        ("zip").style.border = "1px solid red";
+
+    document.getElementById
+        ("name-card").style.border = "1px solid red";
+
+    document.getElementById
+        ("credit-card-num").style.border = "1px solid red";
+
+    document.getElementById
+        ("exp-month").style.border = "1px solid red";
+
+    document.getElementById
+        ("exp-year").style.border = "1px solid red";
+
+    document.getElementById
+        ("cvv").style.border = "1px solid red";
+}
+
 document.querySelector(".button").addEventListener("click", function (event) {
     event.preventDefault();
     stt++;
@@ -54,55 +92,49 @@ document.querySelector(".button").addEventListener("click", function (event) {
     if (check.checked != true) {
         alert("must check");
     } else {
-        let fullName = document.forms["my-form"]["full-name"].value;
-        let email = document.forms["my-form"]["email"].value;
-        let address = document.forms["my-form"]["address"].value;
-        let city = document.forms["my-form"]["city"].value;
-        let state = document.forms["my-form"]["state"].value;
-        let zip = document.forms["my-form"]["zip"].value;
-        let nameCard = document.forms["my-form"]["name-card"].value;
-        let creditCardNum = document.forms["my-form"]["credit-card-num"].value;
-        let expMonth = document.forms["my-form"]["exp-month"].value;
-        let expYear = document.forms["my-form"]["exp-year"].value;
-        let cvv = document.forms["my-form"]["CVV"].value;
-
-        let rs = fullName + " / " + email + " / " + address
-            + " / " + city + " / " + state + " / " +
-            zip + " / " + nameCard + " / " + creditCardNum
-            + " / " + expMonth + " / " + expYear + " / " + cvv;
-
-        let arrOfValue = [fullName, email, address, city,
-            state, zip, nameCard, creditCardNum, expMonth, expYear, cvv];
-
-
-
-        row_1.appendChild(heading_1);
-        row_1.appendChild(heading_2);
-        row_1.appendChild(heading_3);
-        row_1.appendChild(heading_4);
-        row_1.appendChild(heading_5);
-        row_1.appendChild(heading_6);
-        row_1.appendChild(heading_7);
-        row_1.appendChild(heading_8);
-        row_1.appendChild(heading_9);
-        row_1.appendChild(heading_10);
-        row_1.appendChild(heading_11);
-        row_1.appendChild(heading_12);
-        thead.appendChild(row_1);
-
-
-        // Creating and adding data to second row of the table
-        let row_2 = document.createElement('tr');
-        let row_2_col_1 = document.createElement('td');
-        row_2_col_1.innerHTML = stt;
-        row_2.appendChild(row_2_col_1);
-
-        for (let i = 0; i < 11; i++) {
-            let col = document.createElement('td');
-            col.innerHTML = arrOfValue[i];
-            row_2.appendChild(col);
+        // let rs = fullName + " / " + email + " / " + address
+        //     + " / " + city + " / " + state + " / " +
+        //     zip + " / " + nameCard + " / " + creditCardNum
+        //     + " / " + expMonth + " / " + expYear + " / " + cvv;
+        if (fullName == "" || email == "" || zip == "" || nameCard == ""
+            || creditCardNum == "" || expMonth == "" || expYear == ""
+            || cvv == "") {
+            errForm();
         }
-        tbody.appendChild(row_2);
+        else {
+            let arrOfValue = [fullName, email, address, city,
+                state, zip, nameCard, creditCardNum, expMonth, expYear, cvv];
+
+
+
+            row_1.appendChild(heading_1);
+            row_1.appendChild(heading_2);
+            row_1.appendChild(heading_3);
+            row_1.appendChild(heading_4);
+            row_1.appendChild(heading_5);
+            row_1.appendChild(heading_6);
+            row_1.appendChild(heading_7);
+            row_1.appendChild(heading_8);
+            row_1.appendChild(heading_9);
+            row_1.appendChild(heading_10);
+            row_1.appendChild(heading_11);
+            row_1.appendChild(heading_12);
+            thead.appendChild(row_1);
+
+
+            // Creating and adding data to second row of the table
+            let row_2 = document.createElement('tr');
+            let row_2_col_1 = document.createElement('td');
+            row_2_col_1.innerHTML = stt;
+            row_2.appendChild(row_2_col_1);
+
+            for (let i = 0; i < 11; i++) {
+                let col = document.createElement('td');
+                col.innerHTML = arrOfValue[i];
+                row_2.appendChild(col);
+            }
+            tbody.appendChild(row_2);
+        }
 
     }
 
