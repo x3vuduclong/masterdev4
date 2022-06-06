19283cdb4dd07d3f601216fb68af20ebc8041d77
vietdq45 <vietdoan221099@gmail.com>
2022-06-06 14:29:22 +0700

    update validate

--- logic.js
+++ logic.js
@@ -1,3 +1,19 @@
+const fullName = document.getElementById("fname");
+const email = document.getElementById("email");
+const address = document.getElementById("address");
+const city = document.getElementById("city");
+const state = document.getElementById("state");
+const zip = document.getElementById("zip");
+const nameOfCard = document.getElementById("nameOfCard");
+const creditCardNum = document.getElementById("creditCardNum");
+const expMonth = document.getElementById("expMonth");
+const expYear = document.getElementById("expYear");
+const cvv = document.getElementById("cvv");
+
+const btnSubmit = document.getElementById("btn-submit");
+
+const inputEle = document.querySelectorAll(".input-row");
+
 let table = document.createElement('table');
 let thead = document.createElement('thead');
 let tbody = document.createElement('tbody');
@@ -44,37 +60,141 @@ row_1.appendChild(heading_10);
 row_1.appendChild(heading_11);
 thead.appendChild(row_1);
 
-// function validateForm(){
-//     let fullName = document.forms["myForm"]["fname"].value;
-//     let email = document.forms["myForm"]["email"].value;
-//     let address = document.forms["myForm"]["address"].value;
-//     let city = document.forms["myForm"]["city"].value;
-//     let state = document.forms["myForm"]["state"].value;
-//     let zip = document.forms["myForm"]["zip"].value;
-//     let nameOfCard =  document.forms["myForm"]["nameOfCard"].value;
-//     let creditCardNum = document.forms["myForm"]["creditCardNum"].value;
-//     let expMonth = document.forms["myForm"]["expMonth"].value;
-//     let expYear = document.forms["myForm"]["expYear"].value;
-//     let cvv = document.forms["myForm"]["cvv"].value;
-// }
-function submitButton(){
-    let fullName = document.getElementById("fname").value;
-    let email = document.getElementById("email").value;
-    let address = document.getElementById("address").value;
-    let city = document.getElementById("city").value;
-    let state = document.getElementById("state").value;
-    let zip = document.getElementById("zip").value;
-    let nameOfCard = document.getElementById("nameOfCard").value;
-    let creditCardNum = document.getElementById("creditCardNum").value;
-    let expMonth = document.getElementById("expMonth").value;
-    let expYear = document.getElementById("expYear").value;
-    let cvv = document.getElementById("cvv").value;
-    let value = [fullName,email,address,city,state,zip,nameOfCard,creditCardNum,expMonth,expYear,cvv];
-    let row_2 = document.createElement('tr');
-    for(let i=0;i<11;i++){
-        let row_2_data_1 = document.createElement('td');
-        row_2_data_1.innerHTML = value[i];
-        row_2.appendChild(row_2_data_1);
-    }  
-    tbody.append(row_2);
+btnSubmit.addEventListener("click", function () {
+    Array.from(inputEle).map((ele) =>
+        ele.classList.remove("success", "error")
+    );
+    let isValid = checkValidate();
+
+    if (isValid == true) {
+        let fullNameValue = fullName.value;
+        let emailValue = email.value;
+        let addressValue = address.value;
+        let cityValue = city.value;
+        let stateValue = state.value;
+        let zipValue = zip.value;
+        let nameOfCardValue = nameOfCard.value;
+        let creditCardNumValue = creditCardNum.value;
+        let expMonthValue = expMonth.value;
+        let expYearValue = expYear.value;
+        let cvvValue = cvv.value;
+        let value = [fullNameValue, emailValue, addressValue, cityValue, stateValue, zipValue, nameOfCardValue, creditCardNumValue,
+            expMonthValue, expYearValue, cvvValue
+        ];
+        let row_2 = document.createElement('tr');
+        for (let i = 0; i < value.length; i++) {
+            let row_2_data_1 = document.createElement('td');
+            row_2_data_1.innerHTML = value[i];
+            row_2.appendChild(row_2_data_1);
+        }
+        tbody.append(row_2);
+    }
+})  ;
+
+
+function setError(ele, message) {
+    let parentEle = ele.parentNode;
+    parentEle.classList.add("error");
+    parentEle.querySelector("small").innerText = message;
 }
+
+function setSuccess(ele) {
+    ele.parentNode.classList.add("success");
+}
+
+function checkValidate() {
+    let fullNameValue = fullName.value;
+    let emailValue = email.value;
+    let addressValue = address.value;
+    let cityValue = city.value;
+    let stateValue = state.value;
+    let zipValue = zip.value;
+    let nameOfCardValue = nameOfCard.value;
+    let creditCardNumValue = creditCardNum.value;
+    let expMonthValue = expMonth.value;
+    let expYearValue = expYear.value;
+    let cvvValue = cvv.value;
+
+    let isCheck = true;
+    //check fullName
+    if (fullNameValue == "") {
+        setError(fullName, "fullName is empty");
+        isCheck = false;
+    } else {
+        setSuccess(fullName);
+    }
+
+    //check email
+    if (emailValue == "") {
+        setError(email, "Email is empty");
+        isCheck = false;
+    } else {
+        setSuccess(email);
+    }
+
+    if (addressValue == "") {
+        setError(address, "Address is empty");
+        isCheck = false;
+    } else {
+        setSuccess(address);
+    }
+
+    if (cityValue == "") {
+        setError(city, "City is empty");
+        isCheck = false;
+    } else {
+        setSuccess(city);
+    }
+
+    if (stateValue == "") {
+        setError(state, "State is empty");
+        isCheck = false;
+    } else {
+        setSuccess(state);
+    }
+
+    if (zipValue == "") {
+        setError(zip, "Zip is empty");
+        isCheck = false;
+    } else {
+        setSuccess(zip);
+    }
+
+    if (nameOfCardValue == "") {
+        setError(nameOfCard, "nameOfCard is empty");
+        isCheck = false;
+    } else {
+        setSuccess(nameOfCard);
+    }
+
+    if (creditCardNumValue == "") {
+        setError(creditCardNum, "creditCardNumber is empty");
+        isCheck = false;
+    } else {
+        setSuccess(creditCardNum);
+    }
+
+    if (expMonthValue == "") {
+        setError(expMonth, "expMonth is empty");
+        isCheck = false;
+    } else {
+        setSuccess(expMonth);
+    }
+
+    if (expYearValue == "") {
+        setError(expYear, "ExpYear is empty");
+        isCheck = false;
+    } else {
+        setSuccess(expYear);
+    }
+
+    if (cvvValue == "") {
+        setError(cvv, "CVV is empty");
+        isCheck = false;
+    } else {
+        setSuccess(cvv);
+    }
+    return isCheck;
+}
+
+
